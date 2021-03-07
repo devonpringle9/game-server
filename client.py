01089f0a8f407a7ed683cc68b093062454b42eff
devonpringle9 <devonpringle9@gmail.com>
2021-03-07 11:26:39 +1000

    Make server threaded
    The server is now threaded and can now service more than one player. The server will stay alive while the client currently ends and doesn't await further requests from the server.
    
    This commit also adds the following functionality to the server and client. They can now connect and handle transferring a game object and successfully load it.

--- client.py
+++ client.py
@@ -1,8 +1,34 @@
 import socket
 import sys
 import json
+import hive.game as hive
+import time
 
-HOST, PORT = "localhost", 9000
+DEBUG = 1
+HOST, PORT = "localhost", 9002
+
+
+def receive_json(sock, recv_size=1024):
+    """ Wrapper for receiving a json from the server """
+    try:
+        received = str(sock.recv(recv_size), 'utf-8')
+    except Exception as e:
+        raise Exception(f"Got an error when trying to receive: {e}")
+        return None
+
+    json_recv = None
+    try:
+        json_recv = json.loads(received)
+        if DEBUG: print("Received json:", json_recv, type(json_recv))
+    except Exception as e:
+        raise Exception(f"Didnt receive valid json: {received}")
+
+    return json_recv
+
+def send_json(sock, json_to_send):
+    """ Wrapper for easily sending a json (dict) """
+    print("Sending:", json_to_send)
+    sock.sendall(bytes(json.dumps(json_to_send) + "\n", "utf-8"))
 
 
 def print_board(players):
@@ -14,26 +40,49 @@ def print_board(players):
         print(player.token)
 
 
-def connect(sock):
-    data = {'connect': True}
-    sock.sendall(bytes(json.dumps(data) + "\n", "utf-8"))
+def connect_to_game_server(sock):
+    """ Connect to the server to know we want to play and get an id from the server """
+    data = {'request': 'connect'}
+    send_json(sock, data)
+    received_json = receive_json(sock)
+    return received_json['client_id']
+
+
+def request_game(sock):
+    """ Get the game json and this client's player in the game """
+    data = {'request': 'get_game'}
+    send_json(sock, data)
+    received_json = receive_json(sock, recv_size=16384)
+
+    json.loads(received_json['game'])
+
+    
+    return json.loads(json.loads(received_json['game'])), received_json['player_id']
+
 
 
 if __name__ == "__main__":
+
+    # A game for this client to use
+    game = hive.Game()
+
     # Create a socket (SOCK_STREAM means a TCP socket)
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         # Connect to server and send data
         sock.connect((HOST, PORT))
-        player_number = connect(sock)
+        player_number = connect_to_game_server(sock)
+        print(f"You are connected with client_id: {player_number}")
+
+        # Get the game to create a game instance
+        game_json, player_id = request_game(sock)
+
+        game.import_json(game_json)
+
 
         # Everytime we are asked for a move, lets give it to them
-        request_count = 0
-        while request_count < 1:
-            received = str(sock.recv(1024), "utf-8")
-            try:
-                json_recv = json.loads(received)
-                print("got this", json_recv)
-            except Exception:
-                print("Didnt receive json")
-                print(received, '...nothing')
-            request_count += 1
+        # request_count = 0
+        # while request_count < 4:
+        #     print(connect_to_game_server(sock))
+        #     request_count += 1
+        #     time.sleep(1)
+        # time.sleep(4)
--- server.py
+++ server.py
@@ -1,43 +1,85 @@
 import socketserver
 import json
+import threading
+import time
+import client
+import hive.game as hive
 
 server_address = "localhost" #"192.168.0.22"
-server_port = 9000
 
 
-class MyServer(socketserver.BaseRequestHandler):
-
+class HiveClientHandler(socketserver.BaseRequestHandler):
     server_players = 0
 
     def handle(self):
-        self.data = self.request.recv(1024).strip().decode('utf-8')
-        try:
-            json_recv = json.loads(self.data)
-            if json_recv.get('connect'):
-                reply = {'your_player_number': self.server_players}
-                server_players += 1
-                self.request.sendall(json.dumps(reply).encode('utf-8'))
-        except Exception as e:
-            print("wasnt json", e, 'was the error', self.data)
+        """ Main handler. Understands all the actions for each request. """
+        sock = self.request
+        connected = True
+        while connected:
+            json_recv = client.receive_json(sock)
+
+            # If the client wants something from us they will begin with a request
+            if json_recv.get('request'):
+
+                # Give the client and id so we can keep track of them
+                if json_recv['request'] == 'connect':
+                    reply = {'client_id': self.server_players}
+                    client.send_json(sock, reply)
+                    self.server_players += 1
+
+                # Give the client the game and tell them which player they are
+                elif json_recv['request'] == 'get_game':
+                    # TODO: give the client the game
+                    
+                    reply = {
+                             'game': json.dumps(server_game.export_json()),
+                             'player_id': 0
+                            }
+                    client.send_json(sock, reply)
 
 
-        print("{} wrote:".format(self.client_address[0]))
-        print(self.data)
+class HiveServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    pass
+
 
 if __name__ == "__main__":
-    host, port = server_address, server_port
+    host, port = server_address, client.PORT
 
-    with socketserver.TCPServer((host, port), MyServer) as server:
-        server.serve_forever()
+    # Create a game for this server to use. Only one game can be played in one server like this.
+    server_game = hive.Game()
+
+    with HiveServer((host, port), HiveClientHandler) as server:
+        # Start a thread with the server -- that thread will then start one
+        # more thread for each request
+        server_thread = threading.Thread(target=server.serve_forever)
+        # Exit the server thread when the main thread terminates
+        server_thread.daemon = True
+        server_thread.start()
+        print("Server loop running in thread:", server_thread.name)
+
+        while True:
+            print(f"Server still alive. Process sleeping for 60secs")
+            time.sleep(60)
 
 
 
 
 """
-Client sends:
-{"connect": true}
+Client sends connect
+{'request': 'connect'}
 Server sends:
-{'your_player_number': 0} # this is the id of the client and is unique in the server
-Server sends:
-json of the game + whos turn it is
+{'client_id': 0} # this is the id of the client and is unique in the server
+
+Client sends request game
+{'request': 'get_game'}
+Server sends
+# the game json and the player that client is in the game
+{'game': game_json, 'player_id': player_id}
+
+Server sends request move
+{'request': 'move'}
+Client sends
+{'move': move_json}
+Server sends
+{'move': 'valid'}
 """
